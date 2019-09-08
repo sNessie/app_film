441d3325732b8b7fbbedad88ef79533d0d7fd83e
sNessie <a.czajczyk@icloud.com>
2019-09-08 13:56:33 +0200

    rewrite class Person to Main

--- src/Film.java
+++ src/Film.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Film {
     private String title;
@@ -48,27 +46,13 @@ public class Film {
 
     }
 
-    static Film addFilm() {
-        List<String> film = new ArrayList<>(4);
-        System.out.println("Podaj tytuł filmu");
-        Scanner input = new Scanner(System.in);
-        film.add(input.nextLine());
-        System.out.println("Podaj rok produkcji");
-        film.add(input.nextLine());
-        System.out.println("Podaj dyrektora");
-        film.add(input.nextLine());
-        System.out.println("Wybierz kategorię:");
-        for (Genres g : Genres.values()) {
-            System.out.println(g);
-        }
-        film.add(input.nextLine().toUpperCase());
+    static Film addNewFilm(List<String> film) {
         Film newFilm = new FilmBuilder()
                 .title(film.get(0))
                 .year(Integer.parseInt(film.get(1)))
                 .director(film.get(2))
                 .genres(Genres.valueOf(film.get(3)))
                 .build();
-
         return newFilm;
     }
 
